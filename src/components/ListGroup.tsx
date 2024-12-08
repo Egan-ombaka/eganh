2ba775fa73fa259a479fc9cf5d1953b4e7507191
egan-ombaka <eganombaka97@gmail.com>
2024-12-08 19:19:19 +0300

    eganh.dev

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,19 @@
+import { Fragment } from "react/jsx-runtime";
+
 function ListGroup() {
+  let items = ["Nairobi", "Tokyo", "Los Angeles", "Paris", "London"];
+  items = [];
+
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <Fragment>
+      <h1>List</h1>
+      { items.length === 0 ? <p>No items found</p> : null}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </Fragment>
   );
 }
 
