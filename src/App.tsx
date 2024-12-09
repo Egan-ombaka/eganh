c58948b7a7119b3cc8c87bd1dc8291a566f1c1fc
egan-ombaka <eganombaka97@gmail.com>
2024-12-09 16:08:25 +0300

    eganh.dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import ListGroup from "./components/ListGroup";
+import NavBar from "./components/navbar";
 
 function App(){
   return <div><ListGroup /></div>
+  return <div><NavBar /></div>
 }
 
 export default App;
