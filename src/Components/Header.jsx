6698b13f7d22cebdf65502a116a6b7d3599e0d3b
egan-ombaka <eganombaka97@gmail.com>
2024-12-16 07:49:15 +0300

    eganh

---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='headersection'>
+      <p>eeee
+        eeeeeeeeeeeeffffffffff</p>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/Components/Info.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Info.css'
+
+const Info = () => {
+  return (
+    <div className='infosection'>
+      <h1>eeeeeeeeeeeee</h1>
+    </div>
+  )
+}
+
+export default Info
